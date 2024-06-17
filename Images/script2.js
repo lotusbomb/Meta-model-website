07a04f0401d0143e25251847d7091dd035aca490
Onyinyechi Favour <chukwumafavour600@gmail.com>
2024-06-17 07:52:40 +0100

    first commit

---
+++ Images/script2.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const menuBtn = document.getElementById('menu-btn');
+//     const sidebar = document.querySelector('.sidebar');
+
+//     menuBtn.addEventListener('click', function () {
+//         sidebar.classList.toggle('active');
+//     });
+// });
